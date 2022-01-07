96c6adcde74b90660f4a78c16f50ef756a3ec9dd
mohammad6vakili <59134745+mohammad6vakili@users.noreply.github.com>
2022-01-07 18:25:49 +0330

    initial project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'antd/dist/antd.css';
+import 'react-toastify/dist/ReactToastify.css';
+import {Provider} from "react-redux";
+import { ToastContainer } from 'react-toastify';
+import thunk from "redux-thunk";
+import {createStore, combineReducers, applyMiddleware} from "redux";
+import Reducer from "./Store/Reducer";
+import { BrowserRouter } from 'react-router-dom';
+
+
+const rootReducer=combineReducers({
+  Reducer:Reducer
+});
+
+const store = createStore(rootReducer , applyMiddleware(thunk));
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Provider store={store}>
+      <ToastContainer rtl autoClose={6000} pauseOnFocusLoss={false}/>
+      <App />
+    </Provider>
+  </BrowserRouter>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
