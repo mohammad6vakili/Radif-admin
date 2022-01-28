c7124d224e3022432b037551af5ada3d4660868a
mohammad6vakili <59134745+mohammad6vakili@users.noreply.github.com>
2022-01-28 12:25:45 +0330

    messages and report page

--- src/Pages/Dashboard/Messages.jsx
+++ src/Pages/Dashboard/Messages.jsx
@@ -1,11 +1,97 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Messages.css";
+import DatePicker from 'react-modern-calendar-datepicker';
+import grayCalendar from "../../assets/images/GrayCalendar.svg";
+import { SearchOutlined } from '@ant-design/icons';
+import clockIcon from "../../assets/images/clock.svg";
+import trashIcon from "../../assets/images/trash.svg";
+import ticketIcon from "../../assets/images/ticket.svg";
+import logo from "../../assets/images/logo.svg";
+import { Input , Modal , Button} from 'antd';
+import { toast } from 'react-toastify';
+
 
 
 const Messages=()=>{
+    const [modal , setModal]=useState(false);
+    const array=[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
+    const [selectedDayRange, setSelectedDayRange] = useState({
+        from: null,
+        to: null
+      });
     return(
         <div className='messages'>
-            messages
+            <Modal
+                visible={modal}
+                onCancel={()=>setModal()}
+                onOk={()=>setModal(false)}
+                closable={false}
+            >
+                <div className='modal'>
+                    <div className='modal-logo'>
+                        <img src={logo} alt="logo" />
+                    </div>
+                    <div style={{color:"#475569",fontSize:"18px"}}>حذف پیام</div>
+                    <div style={{width:"100%",textAlign:"center",marginTop:"10px"}}>آیا از حذف پیام اطمینان دارید؟</div>
+                    <div className='messages-modal-button'>
+                        <Button onClick={()=>setModal(false)}>انصراف</Button>
+                        <Button onClick={()=>{
+                            toast.success("پیام مورد نظر با موفقیت حذف شد",{
+                                position:"bottom-left"
+                            });
+                            setModal(false);
+                        }}>حذف کردن</Button>
+                    </div>
+                </div>
+            </Modal>
+            <div className="messages-header">
+                <div className='message-header-right'>پیام ها</div>
+                <div className='message-header-left'>
+                    <div style={{position:"relative"}}>
+                        <DatePicker
+                            value={selectedDayRange}
+                            inputClassName="amar-date-picker"
+                            onChange={setSelectedDayRange}
+                            calendarClassName="amar-calendar"
+                            inputPlaceholder="تا تاریخ"
+                            colorPrimary="#f1910c"
+                            colorPrimaryLight="#f1910c50"
+                            shouldHighlightWeekends
+                            locale="fa"
+                        />
+                        <img style={{position:"absolute",right:"80%",top:"20%",zIndex:"9999"}} src={grayCalendar} alt="calendar" />
+                    </div>
+                    <div style={{position:"relative"}}>
+                        <Input
+                            className='message-header-input'
+                            placeholder='چیزی جستجو کنید...'
+                            suffix={<SearchOutlined style={{color:"gray",fontSize:"22px"}} />}
+                        />
+                    </div>
+                </div>
+            </div>
+            <div className='message-items-body'>
+                <div>
+                    <div>
+                        ۱۴۰۰/۱۲/۲۸
+                    </div>
+                </div>
+                {array.map((data)=>(
+                    <div className='message-item'>
+                        <img src={clockIcon} alt="time" />
+                        <div style={{direction:"ltr",fontSize:"15px",margin:"0 10px"}}>۰۹ : ۲۸</div>
+                        <div style={{marginRight:"20px"}}>عباس جعفری با شماره رزرو 2145 جهت افتتاح حساب وارد مرکز شد.</div>
+                        <div className='message-item-icons'>
+                            <div>
+                                <img src={ticketIcon} alt="message" />
+                            </div>
+                            <div onClick={()=>setModal(true)}>
+                                <img src={trashIcon} alt="trash" />
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
--- src/Pages/Dashboard/Report.jsx
+++ src/Pages/Dashboard/Report.jsx
@@ -1,11 +1,89 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Report.css";
+import DatePicker from 'react-modern-calendar-datepicker';
+import grayCalendar from "../../assets/images/GrayCalendar.svg";
+import { Select , Button } from 'antd';
+import FormatHelper from "../../Helper/FormatHelper";
+import Chart from "react-apexcharts";
+const {Option}=Select;
+
 
 
 const Report=()=>{
+
+    const [fromDate , setFromDate]=useState(null);
+    const [toDate , setToDate]=useState(null);
+    const [branch , setBranch]=useState(null);
+    const [service , setService]=useState(null);
+    const array = [1,1,1,1,1,1,1,1,1,1,1];
+
     return(
         <div className='report'>
-            report
+             <div className="report-header">
+                <div style={{position:"relative"}}>
+                    <DatePicker
+                        value={fromDate}
+                        inputClassName="amar-date-picker"
+                        onChange={setFromDate}
+                        calendarClassName="amar-calendar"
+                        inputPlaceholder="از تاریخ"
+                        colorPrimary="#f1910c"
+                        colorPrimaryLight="#f1910c50"
+                        shouldHighlightWeekends
+                        locale="fa"
+                    />
+                    <img style={{position:"absolute",right:"80%",top:"20%",zIndex:"9999"}} src={grayCalendar} alt="calendar" />                
+                </div>
+                <div style={{position:"relative"}}>
+                    <DatePicker
+                        value={toDate}
+                        inputClassName="amar-date-picker"
+                        onChange={setToDate}
+                        calendarClassName="amar-calendar"
+                        inputPlaceholder="تا تاریخ"
+                        colorPrimary="#f1910c"
+                        colorPrimaryLight="#f1910c50"
+                        shouldHighlightWeekends
+                        locale="fa"
+                    />
+                    <img style={{position:"absolute",right:"80%",top:"20%",zIndex:"9999"}} src={grayCalendar} alt="calendar" />
+                </div>
+                <div className='statistic-header-select-wrapper'>
+                    <Select
+                        placeholder="خدمت"
+                        onChange={(value)=>setService(value)}
+                    >
+                        {array.map((data,index)=>(
+                            <Option value={index}>خدمت {index}</Option>
+                        ))}
+                    </Select>
+                </div>
+                <div className='statistic-header-select-wrapper'>
+                    <Button>جستجو</Button>
+                </div>
+            </div>
+            <div className='report-list-wrapper'>
+                <div className='report-list-header'>
+                    <div className='report-item-10'>نوبت</div>
+                    <div className='report-item-20'>تاریخ</div>
+                    <div className='report-item-25'>خدمت</div>
+                    <div className='report-item-10'>وضعیت</div>
+                    <div className='report-item-10'>نوع نوبت</div>
+                    <div className='report-item-10'>باجه</div>
+                    <div className='report-item-15'>کاربر</div>
+                </div>
+                {array.map((data)=>(
+                    <div className='report-list-item'>
+                        <div className='report-item-10'>۱۲۳۴</div>
+                        <div className='report-item-20'>۱۴۰۰/۰۳/۲۵</div>
+                        <div className='report-item-25'>استعلام و تسویه خلافی خودرو</div>
+                        <div className='report-item-10'>موفق</div>
+                        <div className='report-item-10'>آنلاین</div>
+                        <div className='report-item-10'>۲</div>
+                        <div className='report-item-15'>عباس جعفری</div>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
